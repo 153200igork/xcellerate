29126a56a7579ca6d84364438f353d6dd0ea4be7
kayumba igor <kay.kal.igor@gmail.com>
2019-12-04 18:29:24 +0800

    day03s

---
+++ web 0/day03/c.js
@@ -0,0 +1,23 @@
+for (let i = 1; i<=30; i ++){
+    
+    if(i%15 ==0) 
+    {
+      console.log('Hong Kong')
+    }
+    else if (i%2 ==0){
+        console.log('Even')
+      }
+    else if (i%5 ==0)
+    {
+      console.log('Kong')
+    }
+    else if (i%3==0)
+    {
+      console.log('Hong')
+    } 
+    else
+     {
+      console.log(i)
+    }
+
+  }
